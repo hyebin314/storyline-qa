e56780da07a28f1b0d94814b07180ea05da6367f
hyebin314 <hyebin.kang@playtag.ai>
2026-03-20 03:47:13 +0900

    fix firebase config

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCwWj-Suzvf1p2MZ1kSgxvV0-sfUcXT9Yk",
+  authDomain: "storyline-qa.firebaseapp.com",
+  projectId: "storyline-qa",
+  storageBucket: "storyline-qa.firebasestorage.app",
+  messagingSenderId: "267930617309",
+  appId: "1:267930617309:web:57199d839aa612ae2f6234",
 };
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
